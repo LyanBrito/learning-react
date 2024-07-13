9cedf8c79bd8d03f0596af79c7535043a0f78aac
Lyan <lyanbrito613@gmail.com>
2024-07-13 15:41:06 -0300

    feat:created-note-card

---
+++ src/components/noteApp/NoteCard/NoteCard.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class NoteCard extends Component{
+    render(){
+        return(
+            <section>
+            <header>
+                <h3>Title</h3>
+            </header>
+            <p>Your Note</p>
+        </section>
+        )
+
+    }
+    
+
+}
