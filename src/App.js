8896509d7947bcfef1fc9560349fb5283ba995a3
Lyan <lyanbrito613@gmail.com>
2024-07-23 14:42:10 -0300

    feat:added-components-to-App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import NoteList from "./components/noteApp/NoteList/NoteList";
+import "./App.css"
 import NoteForm from "./components/noteApp/NoteFrom/NoteForm";
 
 function App() {
   return (
-    <section>
-    <NoteForm/>
-    <NoteList/>
+    <section className="body">
+
+      <NoteForm />
+      <NoteList />
     </section>
-);
+  );
 }
 
 export default App;
