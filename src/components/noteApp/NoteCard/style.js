6c92ee17b4e7a97e2d36885373df0abfcc0f881e
Lyan <lyanbrito613@gmail.com>
2024-07-23 14:42:45 -0300

    feat:created-card-style

---
+++ src/components/noteApp/NoteCard/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Card = styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    background-color: aliceblue;
+    width: 272px;
+    height: 250px;
+    border : 1px solid lightslategray;
+    padding: 16px;
+    `
+export const Title = styled.text`
+    background-color: aliceblue;
+    text-align: center;
+    
+
+`
+export const Note = styled.text`
+    background-color: aliceblue;
+    
+
+`
