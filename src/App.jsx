24c4d4d003901137de860ca4780d1e680646e1d6
lyanbrito <lyanbrito613@gmail.com>
2024-08-03 15:52:03 -0300

    feat:changed-app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageHeader from "./Projects/Portifolio/PageHeader/PageHeader"; 
+
+
+// import NoteApp from "./Projects/NotePad/components/NoteApp/NoteApp";
+
+export default function App() {
+  return (
+    <PageHeader />
+
+  )
+}
